3d7ea2d872d1a14dc02495f883706f5018e2491c
Ömer Faruk Koran <@I509844>
2025-04-04 20:44:31 +0200

    Initial commit from Spring Initializr

---
+++ src/test/java/com/tweker/auth/TwekerAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweker.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwekerAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
